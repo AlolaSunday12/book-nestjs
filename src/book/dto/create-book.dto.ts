0b7899eb05cbc6250630e2523dfe877033ae7777
AlolaSunday12 <alolasj@gmail.com>
2025-01-17 23:24:01 +0100

    update create book Dto

--- src/book/dto/create-book.dto.ts
+++ src/book/dto/create-book.dto.ts
@@ -1,10 +1,28 @@
+import { IsEnum, IsNotEmpty, IsNumber, IsString } from 'class-validator';
 import { Category } from '../schemas/book.schema';
 
-export class CreateBookDto{
+export class CreateBookDto {
+  @IsNotEmpty()
+  @IsString()
   title: string;
+
+  @IsNotEmpty()
+  @IsString()
   description: string;
+
+  @IsNotEmpty()
+  @IsString()
   author: string;
+
+  @IsNotEmpty()
+  @IsNumber()
   price: number;
-  category: Category
+
+  @IsNotEmpty()
+  @IsEnum(Category, { message: 'Please type correct Category.' })
+  category: Category;
+
+  @IsNotEmpty()
+  @IsString()
   image: string;
 }
