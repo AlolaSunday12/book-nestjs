ade7d290c7df69b22e37a06dabe05140e128e74d
AlolaSunday12 <alolasj@gmail.com>
2025-01-17 23:24:59 +0100

    update book controller

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -1,7 +1,23 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Get,
+  HttpStatus,
+  NotFoundException,
+  Param,
+  ParseFilePipeBuilder,
+  Post,
+  Put,
+  UploadedFiles,
+  UseInterceptors,
+} from '@nestjs/common';
 import { Book } from './schemas/book.schema';
 import { BookService } from './book.service';
 import { CreateBookDto } from './dto/create-book.dto';
+import { FilesInterceptor } from '@nestjs/platform-express';
+import { multerConfig } from './config/multer.config';
+import { UpdateBookDto } from './dto/update-book.dto';
 
 @Controller('book')
 export class BookController {
@@ -9,14 +25,48 @@ export class BookController {
 
   @Get()
   async getAllBooks(): Promise<Book[]> {
-    return this.bookService.findAll()
+    return this.bookService.findAll();
   }
 
   @Post()
-  async Create(
-    @Body()
-    book: CreateBookDto,
+  @UseInterceptors(FilesInterceptor('file', 10, multerConfig))
+  async createBook(
+    @Body() bookDto: CreateBookDto,
+    @UploadedFiles(
+      new ParseFilePipeBuilder()
+        .addFileTypeValidator({
+          fileType: /(jpg|jpeg|png)$/i, // Case-insensitive
+        })
+        .addMaxSizeValidator({
+          maxSize: 1000 * 1000, // 1MB
+        })
+        .build({
+          errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
+        }),
+    )
+    files: Array<Express.Multer.File>,
+  ): Promise<any> {
+    return this.bookService.createBook(bookDto, files);
+  }
+
+  @Get(':id')
+  async getBookId(@Param('id') id: string): Promise<Book> {
+    return await this.bookService.findById(id);
+  }
+
+  @Put(':id')
+  async updateById(
+    @Param('id') id: string,
+    @Body() bookDto: UpdateBookDto,
   ): Promise<Book> {
-    return await this.bookService.createBook(book);
+    if (!Object.keys(bookDto).length) {
+      throw new BadRequestException('No fields provided for update');
+    }
+    const book = await this.bookService.updateById(id, bookDto);
+
+    if (!book) {
+      throw new NotFoundException('Book not found');
+    }
+    return book;
   }
 }
