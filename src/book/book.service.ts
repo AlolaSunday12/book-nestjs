e28054714e0ac3f37a50f517e1432da2878f9331
AlolaSunday12 <alolasj@gmail.com>
2025-01-17 23:24:37 +0100

    update book service

--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -1,7 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Book } from './schemas/book.schema';
 import * as mongoose from 'mongoose';
+import { CreateBookDto } from './dto/create-book.dto';
+import { UpdateBookDto } from './dto/update-book.dto';
 
 @Injectable()
 export class BookService {
@@ -15,8 +17,39 @@ export class BookService {
     return books;
   }
 
-  async createBook(book: Book): Promise<Book> {
-    const books = await this.bookModel.create(book)
-    return books;
+  async createBook(
+    bookDto: CreateBookDto,
+    files: Array<Express.Multer.File>,
+  ): Promise<Book> {
+    const imageUrls = files.map((file) => {
+      return `http://localhost:3000/uploads/${file.filename}`;
+    });
+
+    // Create the book with the DTO and the uploaded image URLs
+    const book = new this.bookModel({
+      ...bookDto,
+      images: imageUrls, // Assuming the schema has an "images" field
+    });
+    console.log('Book to be saved:', book);
+
+    // Save the book in the database
+    return await book.save();
+  }
+
+  async findById(id: string): Promise<Book> {
+    const book = await this.bookModel.findById(id);
+
+    if (!book) {
+      throw new NotFoundException('Book not found');
+    }
+    return book;
+  }
+
+  async updateById(id: string, bookDto: UpdateBookDto): Promise<Book> {
+    const book = await this.bookModel.findByIdAndUpdate(id, bookDto, {
+      new: true,
+      runValidators: true,
+    });
+    return book;
   }
 }
