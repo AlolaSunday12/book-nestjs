e9ba0a1d7ea938036ea048722e3d64a77033a42b
AlolaSunday12 <alolasj@gmail.com>
2025-01-31 23:56:10 +0100

    update on main.ts file

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as express from 'express';
 import { join } from 'path';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,  // Enables automatic transformation
+      whitelist: true,  // Removes unexpected fields
+      forbidNonWhitelisted: true, // Throws an error for unexpected fields
+    }),
+  );
 
   app.use('/uploads', express.static(join(__dirname, '..', 'public/uploads')));
   await app.listen(process.env.PORT ?? 3000);
