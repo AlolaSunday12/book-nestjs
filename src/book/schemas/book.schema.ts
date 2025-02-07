c29885b2a9037fdaa67d02af79702c761861bce6
AlolaSunday12 <alolasj@gmail.com>
2025-02-07 22:21:27 +0100

    update on book schema

--- src/book/schemas/book.schema.ts
+++ src/book/schemas/book.schema.ts
@@ -1,4 +1,6 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { User } from '../../auth/schemas/user.schema';
+import mongoose from 'mongoose';
 
 export enum Category {
   ADVENTURE = 'Adventure',
@@ -28,6 +30,9 @@ export class Book {
 
   @Prop([String])
   images: string[];
+
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User' })
+  user: User;
 }
 
 export const BookSchema = SchemaFactory.createForClass(Book);
