2001c82f3ec5b85f99d6fdfc95860852d9702c34
AlolaSunday12 <alolasj@gmail.com>
2025-02-07 22:21:56 +0100

    update on update book

--- src/book/dto/update-book.dto.ts
+++ src/book/dto/update-book.dto.ts
@@ -1,6 +1,13 @@
-import { IsEnum, IsNumber, IsOptional, IsString } from 'class-validator';
+import {
+  IsEmpty,
+  IsEnum,
+  IsNumber,
+  IsOptional,
+  IsString,
+} from 'class-validator';
 import { Category } from '../schemas/book.schema';
 import { Transform } from 'class-transformer';
+import { User } from '../../auth/schemas/user.schema';
 
 export class UpdateBookDto {
   @IsOptional()
@@ -27,4 +34,7 @@ export class UpdateBookDto {
   @IsOptional()
   @IsString({ each: true }) // Ensures all items in the array are strings
   images?: string[];
+
+  @IsEmpty({ message: 'You can not pass user Id.' })
+  readonly user: User;
 }
