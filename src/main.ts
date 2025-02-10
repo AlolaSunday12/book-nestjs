38e0820eb3f363df705590ad3dc43359cef02b29
AlolaSunday12 <alolasj@gmail.com>
2025-02-10 13:54:43 +0100

    update on main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,15 @@ import { AppModule } from './app.module';
 import * as express from 'express';
 import { join } from 'path';
 import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
+  app.enableCors({
+    origin: '*', // or your frontend domain
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
